e5a18925d8b721e46a54520faa9530944d09026e
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-23 21:00:46 -0700

    Home and vehicle models created, about page created

--- fixit_tracker/models.py
+++ fixit_tracker/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    xp = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Home(models.Model):
+    home_address = models.CharField(max_length=100)
+    home_sqft = models.IntegerField()
+    home_bedrooms = models.IntegerField()
+    home_bathrooms = models.IntegerField()
+    home_acquired = models.DateField()
+    home_photo = models.ImageField(upload_to='home_photos/', blank=True, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.home_address} ({self.profile.user.username})'
+
+class Vehicle(models.Model):
+    vehicle_make = models.CharField(max_length=30)
+    vehicle_model = models.CharField(max_length=30)
+    vehicle_year = models.IntegerField()
+    vehicle_name = models.CharField(max_length=50, blank=True)
+    vehicle_photo = models.ImageField(upload_to='vehicle_photos/', blank=True, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.vehicle_year} {self.vehicle_make} {self.vehicle_model} ({self.profile.user.username})' 
+
--- fixit_tracker/views.py
+++ fixit_tracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+class Home(LoginView):
+    template_name = 'home.html'
+
+# def home(request):
+#     return HttpResponse('<h1>Welcome to FixIt Quest!</h1>')
+
+def about(request):
+    return render(request, 'about.html')
