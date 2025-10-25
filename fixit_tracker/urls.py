d740a6dd3fa274dbbaddf3f10c591d006325f20c
Donovan Campbell <donovan@dlcentertainment.net>
2025-10-24 17:58:16 -0700

    HomeCreate view created, template needed next

--- fixit_tracker/urls.py
+++ fixit_tracker/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
+    path('home/create/', views.HomeCreate.as_view(), name='home-create'),
 ]
--- fixit_tracker/views.py
+++ fixit_tracker/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Profile, Home, Vehicle
 
 # Create your views here.
 class HomeView(LoginView):
     template_name = 'home.html'
-
-# def home(request):
-#     return HttpResponse('<h1>Welcome to FixIt Quest!</h1>')
 
 def about(request):
     return render(request, 'about.html')
@@ -18,3 +16,8 @@ def profile(request):
     homes = Home.objects.filter(profile=profile)
     vehicles = Vehicle.objects.filter(profile=profile)
     return render(request, 'profile/profile.html', {'profile': profile, 'homes': homes, 'vehicles': vehicles })
+
+class HomeCreate(CreateView):
+    model = Home
+    fields = ['home_address', 'home_sqft', 'home_bedrooms', 'home_bathrooms', 'home_acquired', 'home_photo']
+
